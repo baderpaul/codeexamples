058e4ea8fead8f7db1c0eb77a5bab6e9956d4923
baderp <baderp@gmx.de>
2024-07-09 10:36:14 +0200

    [Update]TYPO3 12.4.17, test migration mask slider -> content-block slider

--- config/system/settings.php
+++ config/system/settings.php
@@ -28,7 +28,11 @@ return [
         'ai_suite' => [
             'aiSuiteApiKey' => 'yWYbC5NC0jMwlMzTERiaho842VXSEpPQm4v82mpIM4bgCUdx7WYX5KBXUYrms71y',
             'aiSuiteServer' => 'https://api.autodudes.de/',
+            'googleTranslateApiKey' => '',
             'mediaStorageFolder' => '',
+            'midjourneyApiKey' => '',
+            'midjourneyId' => '',
+            'openAiApiKey' => '',
             'singleNewsDisplayPage' => '1',
             'useUrlForRequest' => '0',
         ],
